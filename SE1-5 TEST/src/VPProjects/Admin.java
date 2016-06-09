4605ebac0c454185870b90ede044e521a9108282
Pranava Rayaprolu <Pranava Rayaprolu@PranavRayaprolu>
2016-06-09 14:37:16 -0500

    initial

---
+++ SE1-5 TEST/src/VPProjects/Admin.java
@@ -0,0 +1,24 @@
+package VPProjects;
+
+public class Admin {
+
+	private String Name;
+	private int Id;
+
+	public String getName() {
+		return this.Name;
+	}
+
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+}
