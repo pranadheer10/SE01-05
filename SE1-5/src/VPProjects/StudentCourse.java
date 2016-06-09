0b33a4e328e6e6c058cf359365f2fa5fbe67f5fb
Pranava Rayaprolu <Pranava Rayaprolu@PranavRayaprolu>
2016-06-09 13:46:37 -0500

    initial commit

---
+++ SE1-5/src/VPProjects/StudentCourse.java
@@ -0,0 +1,23 @@
+package VPProjects;
+
+import java.util.*;
+
+public class StudentCourse {
+
+	ArrayList<Course> courses;
+	private int grade;
+
+	public void getGrade() {
+		// TODO - implement StudentCourse.getGrade
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param grade
+	 */
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+}
